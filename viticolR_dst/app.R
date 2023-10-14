89f502fd7b24b7a04741ea734d1dda16595e62f1
PaulMelloy <paul@melloy.com.au>
2023-10-14 20:25:25 +0000

    ref links, ccs, describe risk

--- viticolR_dst/app.R
+++ viticolR_dst/app.R
@@ -1,10 +1,20 @@
+# This is the non-live version, delete when copying
+# cp -r "/homevol/pmelloy/R/downy_dst/viticolR_dst/"
+#       "/homevol/pmelloy/shiny-server/viticolR_dst/"
+
+
+if("/usr/lib/R/site-library" %in% .libPaths()){
+   .libPaths("/homevol/pmelloy/R/x86_64-pc-linux-gnu-library/4.3")}
 library(shiny)
 library(data.table)
 library(viticolaR)
-library(DT)
+#library(DT)
 library(ggplot2)
+source("R/ccs_styles.R")
 
-load("~/Weather observations/DM_dst_data.rda")
+load("/homevol/pmelloy/Weather observations/DM_dst_data.rda")
+Ddates <- viticolaR::get_PI_dates(DMod)
+
 
 
 # Define UI for application that draws a histogram
@@ -17,7 +27,13 @@ ui <- fluidPage(
                p("A mechanistic compartment model to assist in fungicide decision
                  support for downy mildew (_Plasmodia viticola_) infections in
                  grapevines."),
-
+               h4("Developed from the literature by Dr Paul Melloy, The University of Queensland"),
+               p("Funding for this project was provided through the"),
+               a(href = "https://agriculture-food-sustainability.uq.edu.au/article/2022/03/uq-launches-kickstarter-grants-facilitate-collaboration-industry",
+                 "Agrifood Kickstarter grant"),
+               p("funding in partnership with"),
+               a(href = "https://www.cauldrondistillery.com.au/", "Cauldron distillery"),
+               p(),
                img(src = "grapeleaf_DM_infected.jpg",
                    align = "center",
                    height = "70%",
@@ -35,8 +51,18 @@ ui <- fluidPage(
       tabPanel("Primary disepersals",
                h2("Primary zoospore dispersals from oospore sporangia"),
                h3("North Tamborine mountain"),
-               plotOutput("PI_plot"),
-               p("This plot shows how many 'cohorts' or events where oospores
+               verticalLayout(
+                  ccs_style(1),
+                  code(textOutput(outputId ="txtOosporeGerm" )),
+                  ccs_style(2),
+                  code(textOutput(outputId ="txtDeadSpor")),
+                  ccs_style(3),
+                  code(textOutput(outputId ="txtZooInf")),
+                  ccs_style(3),
+                  code(textOutput(outputId ="txtSporangReady")),
+                  code(textOutput(outputId ="txtlatentPs")),
+                  plotOutput("PI_plot"),
+                  p("This plot shows how many 'cohorts' or events where oospores
                  have germinated sporangia (spo_germination_hour), which have
                  then released zoospores (zoo_release_ind).
                  If a rain event occurs when the zoospores have been released
@@ -45,7 +71,17 @@ ui <- fluidPage(
                  Zoospores can then infect the leaves if conditions remain suitable
                  for infection (zoo_infection_ind).
                  Then the infections are latent for a period before infections
-                 present as oilspots between INC_h_lower and INC_h_upper.")))
+                 present as oilspots between INC_h_lower and INC_h_upper."),
+                 p(""),
+                 tableOutput("PI_table")) # end of verticalLayout
+               ), # end of tabpanel
+      tabPanel("Forecast risk",
+               numericInput("forecast_rain","Forecast rain days in the next week",
+                            min = 0, max = 7,value = 1),
+               code(textOutput(outputId ="txtRisk")),
+
+               )
+      )
 
 
     # Sidebar with a slider input for number of bins
@@ -66,13 +102,30 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
+   # create a vector of sporangia survival lengths to determine likelihood
+   for(i in unique(Ddates$cohort)){
+      if(i == 1) spo_survival <- vector(mode = "numeric")
+      spo_survival <- c(spo_survival,
+                        difftime(Ddates[cohort == i &
+                                           primary_infection_stage == "spo_death_hour",hour],
+                                 Ddates[cohort == i &
+                                           primary_infection_stage == "spo_germination_hour", hour],
+                                 units = "hours")
+      )
+   }
+
    output$img_leaf <- renderImage({
       list(src = "www/grapeleaf_DM_infected.jpg",
            alt = "Grapevine leaf infected with downy mildew")
    },deleteFile = FALSE)
 
-   output$w_table <- renderDataTable(NT_weather)
-   output$PI_table <- renderDataTable(Ddates)
+   # output$w_table <- renderDataTable(NT_weather)
+   output$PI_table <-
+      renderTable(Ddates[(primary_infection_stage == "zoo_dispersal_ind" |
+                               primary_infection_stage == "zoo_infection_ind") &
+                               is.na(hour) == FALSE,list(cohort,
+                                                         primary_infection_stage,
+                                                         Time = as.character(hour))])
 
    # render plot of hydrothermal time
    output$HT_Plot <- renderPlot({
@@ -108,6 +161,71 @@ server <- function(input, output) {
          geom_line()+
          theme_minimal()
    })
+
+   # text descriptions of model output
+   output$txtOosporeGerm <- renderText({
+      paste("Total sporangia germinations this season:",
+            max(Ddates$cohort))
+   })
+
+   zoo_dispersals <- Ddates[primary_infection_stage == "zoo_dispersal_ind" &
+                               is.na(hour)==FALSE,hour]
+   output$txtDeadSpor <- renderText({
+      paste("Sporagia deaths before dispersal:",
+            max(Ddates$cohort) - length(zoo_dispersals))
+   })
+   output$txtZooDisp <- renderText({
+      paste("Zoospore dispersals to vine leaves:",
+            length(zoo_dispersals))
+   })
+   output$txtZooInf <- renderText({
+      paste("Sucessful zoospore infections:",
+            length(Ddates[primary_infection_stage == "zoo_dispersal_ind" &
+                             is.na(hour)==FALSE,hour]))
+   })
+   output$txtSporangReady <- renderText({
+      paste("Current surviving Sporangia cohorts:",
+            length(Ddates[primary_infection_stage == "spo_death_hour" &
+                             is.na(hour),hour]))
+   })
+   output$txtlatentPs <- renderText({
+      #get number of zoospore infections with no infection symptom estimation
+      for(i in unique(Ddates$cohort)){
+         if(i == 1) zoo_inf <- 0
+         zoo_inf <-
+             z_inf <- nrow(Ddates[cohort == i &
+                                     primary_infection_stage == "zoo_infection_ind" &
+                                     is.na(hour) == FALSE])
+         if(z_inf > 0){
+            z_inf <- z_inf *
+               nrow(Ddates[cohort == i &
+                           primary_infection_stage == "INC_h_lower" &
+                           is.na(hour)])
+         }else{
+            z_inf <- 0
+         }
+         zoo_inf <- zoo_inf + z_inf
+      }
+
+      paste("Infections in latent period:",zoo_inf)
+   })
+
+
+   output$txtRisk <- renderText({
+      Sprgia <- length(Ddates[primary_infection_stage == "spo_death_hour" &
+                       is.na(hour),hour])
+      risk_val <- Sprgia * input$forecast_rain
+      risk_txt <- data.table::fcase(risk_val < 1, "Low",
+                                    risk_val >=1 &
+                                       risk_val <4 , "Medium",
+                                    risk_val >=4 &
+                                       risk_val <6, "High",
+                                    risk_val >=6, "Very high",
+                                    default = "Error"
+                                    )
+      paste("Risk of new primary infections:", risk_txt)
+   })
+DMod$cohort_list[[9]]$w_c[indx >= 860 & indx <=890,]
 
 
 }
