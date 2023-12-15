297592ce84aa2842279b304a69a3185dacccfaad
PaulMelloy <paul@melloy.com.au>
2023-12-15 14:08:27 +0000

    Merge other stations

--- R/ObtainWeatherHistory.R
+++ R/ObtainWeatherHistory.R
@@ -15,3 +15,21 @@ merge_axf_weather(File_compressed = paste0("/homevol/pmelloy/Weather observation
    )
 
 }
+
+# Do for Mildura airport
+# find files with desired weather files
+zipfiles <- list.files("/homevol/pmelloy/Weather observations/tgz/",pattern = "_IDS60910.tgz")
+
+for(i in zipfiles){
+   # Create weather for Mildura
+   #  Stanthorpe only records 3 hourly
+   merge_axf_weather(File_compressed = paste0("/homevol/pmelloy/Weather observations/tgz/",i),
+                     File_axf = "IDS60910.95687.axf",
+                     File_formatted = "23-23_RenmarkAP.csv",
+                     base_dir = "/homevol/pmelloy/Weather observations/",
+                     verbose = TRUE
+   )
+
+}
+
+
