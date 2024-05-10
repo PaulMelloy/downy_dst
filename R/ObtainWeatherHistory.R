d3f7e6fe35c2d9e1d353c52a518ac3ca8db438bf
PaulMelloy <paul@melloy.com.au>
2024-05-10 03:17:49 +0000

    add witchcliff

--- R/ObtainWeatherHistory.R
+++ R/ObtainWeatherHistory.R
@@ -2,14 +2,14 @@ library(data.table)
 library(epiphytoolR)
 
 # find files with desired weather files
-zipfiles <- list.files("/homevol/pmelloy/Weather observations/tgz/",pattern = "_IDQ60910.tgz")
+zipfiles <- list.files("/homevol/pmelloy/Weather observations/tgz/",pattern = "_IDW60910.tgz")
 
 for(i in zipfiles){
 # Create weather for Applethorpe
 #  Stanthorpe only records 3 hourly
 merge_axf_weather(File_compressed = paste0("/homevol/pmelloy/Weather observations/tgz/",i),
-                     File_axf = "IDQ60910.94553.axf",
-                     File_formatted = "23-23_Applethorpe.csv",
+                     File_axf = "IDW60910.95641.axf",
+                     File_formatted = "23-24_WitchcliffeWest.csv",
                      base_dir = "/homevol/pmelloy/Weather observations/",
                   verbose = TRUE
    )
