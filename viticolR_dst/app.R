98a5f91eed2ce422e7e4842d35b5209e01082595
PaulMelloy <paul@melloy.com.au>
2025-01-14 22:58:55 +0000

    still can't track

--- viticolR_dst/app.R
+++ viticolR_dst/app.R
@@ -51,7 +51,26 @@ plot_width <- ifelse(length(DMod$time_hours) < 1000,
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   tags$head(includeScript("./viticolR_analytics.js")),
+   tags$head(
+      HTML(
+         "<!-- Matomo -->
+          <script>
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like 'setCustomDimension' should be called before 'trackPageView' */
+            _paq.push(['setDoNotTrack', true]);
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+               var u='https://melloy.matomo.cloud/';
+               _paq.push(['setTrackerUrl', u+'matomo.php']);
+               _paq.push(['setSiteId', '2']);
+               var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+               g.async=true; g.src='https://cdn.matomo.cloud/melloy.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          </script>
+         <!-- End Matomo Code -->"
+      )
+   ),
    # # Styling
    theme = shinytheme("superhero"),
    tags$style('.container-fluid {
--- viticolR_dst/viticolR_analytics.js
+++ viticolR_dst/viticolR_analytics.js
@@ -1,4 +1,3 @@
-<!-- Matomo -->
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(["setDoNotTrack", true]);
@@ -11,4 +10,3 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src='https://cdn.matomo.cloud/melloy.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
   })();
-<!-- End Matomo Code -->
