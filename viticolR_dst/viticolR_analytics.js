d2aea11a1245a69580cf685c72d3095a19c3b685
PaulMelloy <paul@melloy.com.au>
2025-01-15 08:21:32 +1000

    move js to separate file

---
+++ viticolR_dst/viticolR_analytics.js
@@ -0,0 +1,14 @@
+<!-- Matomo -->
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDoNotTrack", true]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://melloy.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/melloy.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+<!-- End Matomo Code -->
